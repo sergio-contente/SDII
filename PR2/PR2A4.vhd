3114531cd9e42db79fd421f9008bac9e5d00ef96
sergio <sergio.mcontente@gmail.com>
2021-09-30 19:29:22 -0300

    EP2 completed

--- PR2/PR2A4.vhd
+++ PR2/PR2A4.vhd
@@ -1,23 +1,23 @@
 library ieee;
 use ieee.numeric_bit.all;
 entity ram is
-	generic(
+	generic (
 		addressSize : natural := 5;
 		wordSize : natural := 8
 	);
-	port(
-		ck , wr : in bit ;
-		addr : in bit_vector ( addressSize − 1 downto 0 ) ;
-		data_i : in bit_vector ( wordSize − 1 downto 0 ) ;
-		data_o : out bit_vector ( wordSize−1 downto 0)
+	port (
+		ck, wr : in bit;
+		addr : in bit_vector(addressSize - 1 downto 0);
+		data_i : in bit_vector(wordSize - 1 downto 0);
+		data_o : out bit_vector(wordSize - 1 downto 0)
 	);
 end ram;
 architecture arch_ram of ram is
-	constant MEM_DEPTH : integer := 2**addressSize;
+constant MEM_DEPTH : integer := 2**addressSize;
 type mem_type is array (0 to MEM_DEPTH-1) of bit_vector(wordSize-1 downto 0);
 signal mem : mem_type;
 begin
-	process(addr, ck, wr)
+	process(ck)
 	begin
 		if (ck'event and ck='1') then
 			if wr = '1' then
