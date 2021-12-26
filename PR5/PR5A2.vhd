3fa362bac3f5aa23dc044be4590646d3323562f5
sergio <sergio.mcontente@gmail.com>
2021-12-26 11:46:32 -0300

    🔖 End of EPs

--- PR5/PR5A2.vhd
+++ PR5/PR5A2.vhd
@@ -1,227 +1,111 @@
-use ieee.numeric_bit.ALL;
-
 entity fulladder is
-  port (
-    a, b, cin: in bit;
-    s, cout: out bit
-  );
-end entity;
-
-architecture structural of fulladder is
-  signal axorb: bit;
-begin
-  axorb <= a xor b;
-  s <= axorb xor cin;
-  cout <= (axorb and cin) or (a and b);
-end architecture;
-
-entity mux_2to1 is
-  port (
-    SEL : in  bit;    
-    A :   in  bit;
-    B :   in  bit;
-    Y :   out bit
+    port (
+        a, b, cin : in bit;
+        s, cout : out bit
     );
-end entity mux_2to1;
+end entity fulladder;
 
-architecture with_select of mux_2to1 is
+architecture wakerly of fulladder is
+-- Solution Wakerly's Book (4th Edition, page 475)
 begin
-  with SEL select
-    Y <= A when '0',
-         B when '1',
-         '0' when others;
-end architecture with_select;
+    s <= (a xor b) xor cin;
+    cout <= (a and b) or (cin and a) or (cin and b);
+end architecture wakerly;
 
-entity mux_4to1 is
-  port (
-    SEL : 		in  bit_vector (1 downto 0);    
-	A,B,C,D : 	in  bit;
-    Y :   		out bit
-    );
-end entity mux_4to1;
-
-architecture with_select of mux_4to1 is
-begin
-  with SEL select
-    Y <= A when "00",
-         B when "01",
-         C when "10",
-         D when "11",
-         '0' when others;
-end architecture with_select;
+library ieee;
+use ieee.numeric_bit.all;
 
 entity alu1bit is
-	port(
-		a, b, less, cin: in bit;
-		result, cout, set, overflow: out bit;
-		ainvert, binvert: in bit;
-		operation : in bit_vector (1 downto 0)
-	);
-end alu1bit;
-
-architecture alu1bit_arch of alu1bit is
-	component fulladder is
-	port (
-		a, b, cin: in bit;
-		s, cout:  out bit
-	);
-	end component;
-	
-	component mux_2to1 is
-	port (
-    	SEL : in  bit;    
-    	A :   in  bit;
-    	B :   in  bit;
-    	Y :   out bit
-    );
-    end component;
-    
-    component mux_4to1 is
     port (
-    	SEL : 		in  bit_vector (1 downto 0);    
-		A,B,C,D : 	in  bit;
-    	Y :   		out bit
+        a, b, less, cin: in bit;
+        result, cout, set, overflow: out bit;
+        ainvert, binvert: in bit;
+        operation: in bit_vector(1 downto 0)
     );
+end entity alu1bit;
+
+architecture alu1bit_structural of alu1bit is
+    component fulladder is
+        port (
+            a, b, cin : in bit;
+            s, cout : out bit
+        );
     end component;
-    	
-   	signal nota, notb,aprep, bprep : bit;
-   	signal andentry, orentry, addentry : bit;
-   	signal coutsignal : bit;
+    signal a_in, b_in: bit;
+    signal fa_result, fa_cout: bit;
 begin
-	nota <= not(a);
-	notb <= not(b);
-	MUXAINV : mux_2to1	port map (
-		SEL => ainvert,
-		A => a ,
-		B => nota,
-		Y => aprep
-	);
-	
-	MUXBINV : mux_2to1	port map (
-		SEL => binvert,
-		A => b ,
-		B => notb,
-		Y => bprep
-	);
-	
-	ADDER : fulladder port map(
-		a => aprep,
-		b => bprep,
-		cin => cin,
-		s => addentry,
-		cout => coutsignal               
-	);
-	
-	andentry <= aprep and bprep;
-	orentry <= aprep or bprep;
-	overflow <= cin xor coutsignal;
-	cout <= coutsignal;
-	set <= addentry;
-	
-	MUXRESULT: mux_4to1 port map(
-		SEL => operation,
-		A => andentry,
-		B => orentry,
-		C => addentry,
-		D => b,
-		Y => result
-	);
-	
-end alu1bit_arch;
+    FA: fulladder port map(a_in, b_in, cin, fa_result, fa_cout);
+
+    with ainvert select
+        a_in <= not(a) when '1',
+                a when others;
+
+    with binvert select
+        b_in <= not(b) when '1',
+                b when others;
+    
+    cout <= fa_cout;
+    set <= fa_result;
+    overflow <= cin xor fa_cout;
+
+    with operation select
+        result <=   a_in and b_in when "00",
+                    a_in or b_in when "01",
+                    fa_result when "10",
+                    b when "11";
+
+
+end architecture alu1bit_structural;
+
+library ieee;
+use ieee.numeric_bit.all;
 
 entity alu is
-	generic (
-		size: natural := 64
-	);
-	port (
-		A,B	: in  bit_vector(size-1 downto 0);
-		F	: out  bit_vector(size-1 downto 0);
-		S	: in  bit_vector(3 downto 0);
-		Z	: out bit;
-		Ov	: out bit;
-		Co	: out bit
-	);
+    generic (
+        size : natural := 64
+    );
+    port (
+        A, B : in bit_vector(size-1 downto 0); -- inputs
+        F : out bit_vector(size-1 downto 0); -- output
+        S : in bit_vector(3 downto 0); -- op selection
+        Z : out bit; -- zero flag
+        Ov : out bit; -- overflow flag
+        Co : out bit -- carry out
+    );
 end entity alu;
 
-architecture alu_arch of alu is
-	component alu1bit is
-	port(
-		a, b, less, cin: in bit;
-		result, cout, set, overflow: out bit;
-		ainvert, binvert: in bit;
-		operation : in bit_vector (1 downto 0)
-	);
-	end component;
-	signal carryvector : bit_vector(size downto 0);
-	signal lessvector, setvector, overflowvector, result : bit_vector(size-1 downto 0);
-	signal singlebitoperation: bit_vector (1 downto 0);
-	signal a_inv_signal, b_inv_signal : bit; 
-	
-begin 
-	process (lessvector,setvector)
-	begin
-   		LESSVECTORFILL: FOR i IN 0 to size-1 LOOP
-			if i = 0 then
-				lessvector(i) <= setvector(size-1);
-			else
-				lessvector(i) <= '0';
-			end if;
-		END LOOP LESSVECTORFILL;
-	end process;
-	
-	PREPAREENTRIES : process(S) is
-	begin
-		if 		S = "0000" then --AND bit by bit
-			singlebitoperation <= "00";
-			a_inv_signal <= '0';
-			b_inv_signal <= '0';
-			carryvector(0) <= '0';
-		elsif 	S = "0001" then --OR bit by bit
-		    singlebitoperation <= "01";
-		    a_inv_signal <= '0';
-			b_inv_signal <= '0';
-			carryvector(0) <= '0';
-		elsif	S = "0010" then	--ADD
-			singlebitoperation <= "10";
-			a_inv_signal <= '0';
-			b_inv_signal <= '0';
-			carryvector(0) <= '0';
-		elsif	S = "0110" then --SUBTRACT
-			singlebitoperation <= "10";
-			a_inv_signal <= '0';
-			b_inv_signal <= '1';
-			carryvector(0) <= '1';
-		elsif 	S = "0111" then --SLT
-			singlebitoperation <= "11";
-			a_inv_signal <= '0';
-			b_inv_signal <= '1';
-			carryvector(0) <= '1';
-		elsif 	S = "1100" then --NOR bit by bit
-			singlebitoperation <= "00";
-			a_inv_signal <= '1';
-			b_inv_signal <= '1';
-			carryvector(0) <= '1'; 
-		end if;
-	end process PREPAREENTRIES;
-	
-	ALUGEN: FOR i IN size-1 downto 0 GENERATE
-		ALURIPPLEi : alu1bit port map(
-		A(i),
-		B(i),
-		lessvector(i),
-		carryvector(i),
-		result(i),
-		carryvector(i+1),
-		setvector(i),
-		overflowvector(i),
-		a_inv_signal,
-		b_inv_signal,
-		singlebitoperation 
-		);
-	END GENERATE ALUGEN;
-	F <= result;
-	Ov <= overflowvector(size-1);
-	Z  <= '1' when result =(result'range => '0') else '0';
-	Co <= carryvector(size); 
-	
-end alu_arch;
+architecture alu_structural of alu is
+    component alu1bit is
+        port(
+            a, b, less, cin: in bit;
+            result, cout, set, overflow: out bit;
+            ainvert, binvert: in bit;
+            operation : in bit_vector(1 downto 0)
+        );
+    end component alu1bit;
+
+    signal carrys, result_in, set_in : bit_vector(size-1 downto 0);
+    signal less_in : bit;
+begin
+
+    ALU_GEN: for i in 0 to (size-1) generate
+        MS_ALU: if (i = (size-1)) generate
+            alu_1b: alu1bit port map(A(i), B(i), '0', carrys(i-1), result_in(i), carrys(i), set_in(i), Ov, S(3), S(2), S(1 downto 0));
+        end generate;
+
+        MD_ALU: if ((i > 0) and (i < (size-1))) generate
+            alu_1b: alu1bit port map(A(i), B(i), '0', carrys(i-1), result_in(i), carrys(i), set_in(i), open, S(3), S(2), S(1 downto 0));
+        end generate;
+
+        LS_ALU: if (i = 0) generate
+            alu_1b: alu1bit port map(A(i), B(i), less_in, S(2), result_in(i), carrys(i), set_in(i), open, S(3), S(2), S(1 downto 0));
+        end generate;
+    end generate;
+    less_in <= set_in(size-1);
+
+    Co <= carrys(size-1);
+    Z <= '1' when (signed(result_in) = 0) else '0';
+
+    F <= result_in;
+
+end architecture alu_structural;
